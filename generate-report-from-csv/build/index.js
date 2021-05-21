3827d4d872b29a47a5bd4604016f868281d6d999
nax2uk <azlinayeo@gmail.com>
2021-05-21 12:57:57 +0100

    added CsvFileReader class

--- generate-report-from-csv/build/index.js
+++ generate-report-from-csv/build/index.js
@@ -1,1 +1,13 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+var manUnitedWins = 0;
+reader.data.forEach(function (match) {
+    if (match[1] == 'Man United' && match[5] == 'H')
+        manUnitedWins++;
+    else if (match[2] == 'Man United' && match[5] == 'A')
+        manUnitedWins++;
+});
+console.log("Man United won " + manUnitedWins + " matches");
---
+++ generate-report-from-csv/src/index.ts
@@ -0,0 +1,12 @@
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+let manUnitedWins = 0;
+reader.data.forEach(match => {
+  if (match[1] == 'Man United' && match[5] == 'H') manUnitedWins ++;
+  else if (match[2] == 'Man United' && match[5] == 'A') manUnitedWins++;
+});
+
+console.log(`Man United won ${manUnitedWins} matches`);
