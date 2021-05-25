c2c0e5f1a7678e3c3a621f1b3deee7d2edcf8af4
nax2uk <azlinayeo@gmail.com>
2021-05-25 10:41:49 +0100

    added MatchReader class and refactored CsvFileReader to be an abstract class

--- generate-report-from-csv/build/index.js
+++ generate-report-from-csv/build/index.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
-var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+var MatchReader_1 = require("./MatchReader");
+var MatchResult_1 = require("./MatchResult");
+var reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
 var manUnitedWins = 0;
 reader.data.forEach(function (match) {
-    if (match[1] == 'Man United' && match[5] == 'H')
+    if (match[1] == 'Man United' && match[5] == MatchResult_1.MatchResult.HomeWin)
         manUnitedWins++;
-    else if (match[2] == 'Man United' && match[5] == 'A')
+    else if (match[2] == 'Man United' && match[5] == MatchResult_1.MatchResult.AwayWin)
         manUnitedWins++;
 });
 console.log("Man United won " + manUnitedWins + " matches");
--- generate-report-from-csv/src/index.ts
+++ generate-report-from-csv/src/index.ts
@@ -1,13 +1,13 @@
-import { CsvFileReader } from './CsvFileReader';
-import { dateStringToDate } from './utils';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
 let manUnitedWins = 0;
 reader.data.forEach(match => {
-  if (match[1] == 'Man United' && match[5] == 'H') manUnitedWins ++;
-  else if (match[2] == 'Man United' && match[5] == 'A') manUnitedWins++;
+  if (match[1] == 'Man United' && match[5] == MatchResult.HomeWin) manUnitedWins ++;
+  else if (match[2] == 'Man United' && match[5] == MatchResult.AwayWin) manUnitedWins++;
 });
 
 console.log(`Man United won ${manUnitedWins} matches`);
