f6ea2c20b35751082266fd291687bdac0e7673f0
nax2uk <azlinayeo@gmail.com>
2021-07-02 17:11:44 +0100

    updated web-framework code

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,5 +1,5 @@
 import { User } from './models/User';
-const user = new User({ id: 1, name: 'newer name', age: 999 });
+const user = User.buildUser({ id: 1 });
 
 user.on('save',() => {
   console.log(user);
